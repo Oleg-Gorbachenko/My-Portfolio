8c6c50e2ef15ba6f838cee6853981f4f8c3e7f58
Oleg <viktorovich959595@mail.ru>
2022-04-03 12:53:51 +1000

    first commit"

---
+++ src/skills/Skills.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Skills.module.css'
+import styleContainer from './../common/styles/Container.module.css'
+import {Skill} from "./skill/Skill";
+
+
+export const Skills = () => {
+    return (
+        <div className={styles.skillsBlock}>
+            <div className={`${styleContainer.container} ${styles.skillsContainer}`}>
+                <h2 className={styles.title}>Skills</h2>
+                <div className={styles.skills}>
+                    <Skill title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt facere impedit iure laborum, nihil placeat quam qui saepe. Accusantium autem, deleniti excepturi illo ipsa mollitia numquam optio sunt vitae voluptatum'}/>
+                    <Skill title={'React'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt facere impedit iure laborum, nihil placeat quam qui saepe. Accusantium autem, deleniti excepturi illo ipsa mollitia numquam optio sunt vitae voluptatum'}/>
+                    <Skill title={'CSS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt facere impedit iure laborum, nihil placeat quam qui saepe. Accusantium autem, deleniti excepturi illo ipsa mollitia numquam optio sunt vitae voluptatum'}/>
+                </div>
+            </div>
+        </div>
+    );
+};
