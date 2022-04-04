95f11934092c89a3c892138ef509cebd08ea5986
Oleg <viktorovich959595@mail.ru>
2022-04-04 21:24:42 +1000

    add contacts module

---
+++ src/contacts/Contacts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Contacts.module.css'
+import styleContainer from "../common/styles/Container.module.css";
+import styleBtn from "../common/styles/Btn.module.css";
+
+export const Contacts = () => {
+    return (
+        <div className={styles.contactsBlock}>
+            <div className={`${styleContainer.container} ${styles.contactsContainer}`}>
+                <h2 className={styles.title}>Contacts</h2>
+                <accept className={styles.forms}>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <textarea name="" id="" cols="40" rows="10"></textarea>
+                </accept>
+                <button className={`${styleBtn.btn} ${styles.btn}`}>Push</button>
+            </div>
+        </div>
+    );
+};
